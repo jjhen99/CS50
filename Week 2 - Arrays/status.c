098091b9d656156a0a6f50e1a102aeb12070772c
jjhen99 <141966475+jjhen99@users.noreply.github.com>
2024-07-15 19:43:15 +0200

    Add files via upload

---
+++ Week 2 - Arrays/status.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//Use echo $?
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+
+    }
+    else
+    {
+        printf("hello, %s\n", argv[1]);
+
+        return 0;
+    }
+}
